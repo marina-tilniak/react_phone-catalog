27211bccc4bbb4b48812ed0aa011a5c0169061b6
Marina Tilniak <marinatilnak@gmail.com>
2022-10-24 12:00:06 +0300

    Updates

---
+++ static/js/main.87f44884.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact_phone-catalog"]=this["webpackJsonpreact_phone-catalog"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var a=c(3),n=c.n(a),s=(c(8),c(0)),o=function(){return Object(s.jsxs)("div",{className:"App",children:[Object(s.jsx)("h1",{children:"React Phone Catalog"}),Object(s.jsx)("p",{className:"withImage",children:"asd"})]})};n.a.render(Object(s.jsx)(o,{}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.87f44884.chunk.js.map
